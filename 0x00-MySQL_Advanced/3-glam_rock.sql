5ba1145b9f3422900b3607baf73efdf88094b766
Chybuzlopez <danielorihanna@gmail.com>
2023-05-04 21:10:33 +0000

    task done

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,5 @@
---SQL script that lists all bands with Glam rock
---as their main style, ranked by their longevity
+-- script that lists all bands with Glam rock as their main style, ranked by their longevity
 SELECT band_name,
-    (IFNULL(split, 2023) - formed) AS lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-GROUP BY band_name,
-    lifespan
-ORDER BY lifespan DESC;
+          (IFNULL(split,2022) - formed) AS lifespan
+	FROM metal_bands
+	WHERE style like '%Glam rock%';
--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,15 +1,12 @@
--- creates a trigger that resets the attribute
--- valid_email only when the email has been changed.
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE IF NOT EXISTS users (
-    id int not null AUTO_INCREMENT,
-    email varchar(255) not null,
-    name varchar(255),
-    valid_email boolean not null default 0,
-    PRIMARY KEY (id)
-);
-
-INSERT INTO users (email, name) VALUES ("bob@dylan.com", "Bob");
-INSERT INTO users (email, name, valid_email) VALUES ("sylvie@dylan.com", "Sylvie", 1);
-INSERT INTO users (email, name, valid_email) VALUES ("jeanne@dylan.com", "Jeanne", 1);
+-- creates a trigger that resets the attribute valid_email only when the email has been changed.
+DROP TRIGGER IF EXISTS reset_valid_email;
+DELIMITER $$
+CREATE TRIGGER reset_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END $$
+DELIMITER ;
